f5915c06fa982a7159d9a2767cff2cdf8f1c657d
hajapy <hajapy@users.noreply.github.com>
2020-08-17 09:25:44 -0400

    Revised approach to version tagging

--- .circleci/test_docker_container.sh
+++ .circleci/test_docker_container.sh
@@ -14,10 +14,10 @@ touch /opt/conda/bin/test_conda_forge
 # check that conda is activated
 conda info
 
-# check that conda version matches
-IFS='-' read -ra miniforge_version_array <<< "$MINIFORGE_VERSION"
-conda_version=${`conda --version`/conda /""}
-if [[ "${miniforge_version_array[0]}" != "${conda_version}" ]]
+# check that conda version matches release tag
+conda_version=$(conda --version)
+conda_version=${conda_version/conda /""}
+if [[ "${RELEASE_CONDA_VERSION}" != "${conda_version}" ]]
  then
   exit 1
 fi
